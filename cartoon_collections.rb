2c313ca7c760a4e34ff6da329ae8e2d080dc3c47
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-15 22:28:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-   planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
+   planeteer_calls.map {|planeteer| planeteer.capitalize + "!"}
   end
 end
 
