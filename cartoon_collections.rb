da6a90e0280412406e651f7156a31e43b12f618f
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-15 22:30:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,6 @@ def roll_call_dwarves(dwarves)
 def summon_captain_planet(planeteer_calls)
    planeteer_calls.map {|planeteer| planeteer.capitalize + "!"}
   end
-end
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? do |call|
